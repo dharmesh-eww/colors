3e39a44586e136de6f93f60341a924069b2ffeb6
Dharmesh Gondaliya <dharmesh.gondaliya@excellentwebworld.in>
2026-07-31 16:16:26 +0530

    feat: add Daily Challenge banner to home screen and clean up unused assets

--- lib/App/core/utils/asset_images.dart
+++ lib/App/core/utils/asset_images.dart
@@ -1,5 +1,4 @@
 class AssetImages {
   static const String _assetpath = "assets/images";
   static const String logo = "$_assetpath/logo.png";
-  static const String empty = "$_assetpath/empty.png";
 }
